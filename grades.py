1922fe7349918fd2994a14d2ceb41d3fbe6cb186
Loïc Séguin-C <loicseguin@gmail.com>
2012-02-10 22:09:43 -0500

    Reorganized headers and footers and added possibility to draw horizontal divisors.

--- grades.py
+++ grades.py
@@ -58,9 +58,10 @@ class GradesTable(object):
         of a list of lines."""
         object.__init__(self)
         self.columns = []
+        self.nb_col_headers = 3
         self.evals = []
         self.students = []
-        self.mean = {}
+        self.footers = []
         # The first three lines contain information about the evaluations.
         self.columns = [entry for entry in self.__parse_line(data[0])
                         if not entry.startswith('-')]
@@ -70,6 +71,7 @@ class GradesTable(object):
                        cname.upper().startswith(('TEST', 'EXAM', 'MIDTERM',
                        'QUIZ', 'FINAL'))]
         eval_names = [self.columns[i] for i in eval_colnum]
+        self.num_columns = list(eval_names)
         eval_max = [self.__to_float(entry) for i, entry in
                     enumerate(self.__parse_line(data[1]))
                     if i in eval_colnum]
@@ -83,7 +85,7 @@ class GradesTable(object):
 
         # The next lines contain student records. Students are stored as a
         # list of dictionaries keyed by column name.
-        for line in data[3:]:
+        for line in data[self.nb_col_headers:]:
             if line.startswith('|-'):
                 # Separator line in the table.
                 continue
@@ -128,22 +130,24 @@ class GradesTable(object):
                          isinstance(student[evalu['name']], (float, int))))
             student['-- Cumul --'] = cumul
         self.columns.append('-- Cumul --')
+        self.num_columns += ['-- Cumul --']
 
     def compute_mean(self):
         """Calculate the class mean for each evaluation and add the results to
         a new row at the bottom of the table. Blanks in the table are not taken
         into account, i.e., a blank does not count as a zero."""
-        self.mean = {}
-        self.mean[self.columns[0]] = '-- Moyenne --'
+        mean = {}
+        mean[self.columns[0]] = '-- Moyenne --'
         for column in self.columns[1:]:
-            self.mean[column] = ''
+            mean[column] = ''
             if column in self.eval_names or column.startswith('--'):
                 nb_students = sum((1 for student in self.students if
                                   isinstance(student[column], (float, int))))
                 s = sum((student[column] for student in self.students
                          if isinstance(student[column], (float, int))))
                 if nb_students:
-                    self.mean[column] = s / nb_students
+                    mean[column] = s / nb_students
+        self.footers.append(mean)
 
 
 class TableWriter(object):
@@ -152,16 +156,9 @@ class TableWriter(object):
         self.padding_left = 1
         self.padding_right = 1
         self.table = grade_table
-        self.num_columns = list(grade_table.eval_names)
-        self.num_rows = list(grade_table.students)
         self.precision = 2
 
-    def printt(self):
-        if ('-- Cumul --' in self.table.columns and not '-- Cumul --' in
-            self.num_columns):
-            self.num_columns += ['-- Cumul --']
-        if (self.table.mean and not self.table.mean in self.num_rows):
-            self.num_rows += [self.table.mean]
+    def printt(self, div_on=['Group', 'Test 1']):
         self.__set_columns_width()
         # Header row.
         str_tbl = self.__row_str(self.table.columns)
@@ -181,31 +178,46 @@ class TableWriter(object):
         str_tbl += self.__row_str(max_row) + self.__row_str(weight_row)
         str_tbl += self.__div_row()
 
+        if div_on:
+            prevs = [self.table.students[0][cname] for cname in div_on]
         for student in self.table.students:
+            if div_on:
+                curs = [student[cname] for cname in div_on]
+                for prev, cur in zip(prevs, curs):
+                    if prev != cur:
+                        str_tbl += self.__div_row()
+                        break
+                prevs = curs
             str_tbl += self.__row_str(
                     (student[cname] for cname in self.table.columns))
 
-        if self.table.mean:
+        if self.table.footers:
             str_tbl += self.__div_row()
-            str_tbl += self.__row_str((self.table.mean[cname] for cname in
-                self.table.columns))
+            for footer in self.table.footers:
+                str_tbl += self.__row_str((footer[cname] for cname in
+                    self.table.columns))
         print(str_tbl)
 
     def __set_columns_width(self):
+        """Find the width of each column. The width of a column is the maximum
+        width of an entry in this column plus the padding."""
         self.column_widths = []
         for column in self.table.columns:
             col = [column]
-            if column in self.num_columns:
-                for student in self.num_rows:
-                    if isinstance(student[column], (float, int)):
-                        col += [format(student[column], '.%df'%self.precision)]
+            if column in self.table.num_columns:
+                for row in self.table.students + self.table.footers:
+                    if isinstance(row[column], (float, int)):
+                        # Numerical data will be formatted to self.precision.
+                        # Calculate the width of the formatted numbers.
+                        col += [format(row[column], '.%df'%self.precision)]
                     else:
-                        col += [student[column]]
+                        # Allow some cells to contain non numerical data such
+                        # as 'ABS' for absences.
+                        col += [row[column]]
             else:
                 col += [student[column] for student in self.table.students]
-            self.column_widths.append(
-                self.padding_left + self.padding_right +
-                max(len(row) for row in col))
+            self.column_widths.append(self.padding_left + self.padding_right +
+                                      max(len(str(row)) for row in col))
 
     def __row_str(self, row):
         """Create a string representation for a row in the table. The columns
@@ -213,7 +225,7 @@ class TableWriter(object):
         padded = []
         for i, rowelmt in enumerate(row):
             width = self.column_widths[i]
-            if (self.table.columns[i] in self.num_columns
+            if (self.table.columns[i] in self.table.num_columns
                 and isinstance(rowelmt, (float, int))):
                 str_elmt = format(rowelmt, '.%df' % self.precision)
                 padded.append(
@@ -228,6 +240,7 @@ class TableWriter(object):
         return '|' + '|'.join(padded) + '|\n'
 
     def __div_row(self):
+        """Return a division that look like |-----+------+------|."""
         div = '|' + '+'.join(('-' * width for width in self.column_widths))
         return div + '|\n'
 
