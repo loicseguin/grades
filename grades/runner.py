0c860b0a51cc054088765c4529e3e941c61b5508
Loïc Séguin-C <loicseguin@gmail.com>
2012-02-13 22:33:07 -0500

    Option to print only the table.

--- grades/runner.py
+++ grades/runner.py
@@ -32,14 +32,16 @@ def run():
             help='print the mean for each evaluation')
     clparser.add_argument('-c', '--cumul', action='store_true',
             help='print the cumulative grade for each student')
-    clparser.add_argument('-g', '--grouped',
-            help='print the mean for each GROUPED for each evaluation; '
-                 + 'GROUPED must be the name of one of the columns in the '
-                 + 'table')
+    clparser.add_argument('-t', '--tableonly', action='store_true',
+            help='print only the table')
     clparser.add_argument('-d', '--div', nargs='*',
             help='draw a horizontal division between students in different '
                  + 'DIV; DIV must be the name of one of the columns in '
                  + 'the table')
+    clparser.add_argument('-g', '--grouped',
+            help='print the mean for each GROUPED for each evaluation; '
+                 + 'GROUPED must be the name of one of the columns in the '
+                 + 'table')
     clparser.add_argument('filehandle', type=argparse.FileType('r'),
             help='grades file to read and parse')
     args = clparser.parse_args()
@@ -51,7 +53,10 @@ def run():
         gfile.table.compute_mean()
     if args.grouped:
         gfile.table.compute_grouped_mean(group_by=args.grouped)
-    gfile.print_file(div_on=args.div)
+    if args.tableonly:
+        gfile.writer.printt(div_on=args.div)
+    else:
+        gfile.print_file(div_on=args.div)
 
 
 if __name__ == '__main__':
